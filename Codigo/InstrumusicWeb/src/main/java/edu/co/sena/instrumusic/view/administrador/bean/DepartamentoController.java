159adbe1b7d1c72204f8a6d16c93f40d591416b5
aprendiz <aprendiz@301_PC_11.corcolsutec.edu.co>
2015-06-04 17:47:52 -0500

    Se el arreglo el modulo departamento.

--- Codigo/InstrumusicWeb/src/main/java/edu/co/sena/instrumusic/view/administrador/bean/DepartamentoController.java
+++ Codigo/InstrumusicWeb/src/main/java/edu/co/sena/instrumusic/view/administrador/bean/DepartamentoController.java
@@ -18,9 +18,10 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.faces.view.ViewScoped;
 
 @Named("departamentoController")
-@SessionScoped
+@ViewScoped
 public class DepartamentoController implements Serializable {
 
     @EJB
