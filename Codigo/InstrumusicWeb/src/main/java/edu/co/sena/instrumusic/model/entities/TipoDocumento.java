3bc44a1119f18a229161aa37761a50ef809396be
JhonAlex133 <jhon_gutierrez@misena.edu.co>
2015-06-08 22:39:50 -0500

    Cuenta al 40%

--- Codigo/InstrumusicWeb/src/main/java/edu/co/sena/instrumusic/model/entities/TipoDocumento.java
+++ Codigo/InstrumusicWeb/src/main/java/edu/co/sena/instrumusic/model/entities/TipoDocumento.java
@@ -133,7 +133,7 @@ public class TipoDocumento implements Serializable {
 
     @Override
     public String toString() {
-        return "edu.co.sena.instrumusic.model.entities.TipoDocumento[ tipoDocumento=" + tipoDocumento + " ]";
+        return " " + tipoDocumento + " ";
     }
     
 }
